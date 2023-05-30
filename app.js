3e29e858fdd60d14d6bfab53548a687c71d8581a
Sagar Devkate <sagardeva1029@gmail.com>
2023-05-29 23:15:09 -0400

    done

--- app.js
+++ app.js
@@ -2,10 +2,37 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
- function clickHandler() {
-    outputDiv.innerText = "dvfbtrbtrbg" + txtInput.value;
-   
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something is wrong with server, try again after sometime!");
+}
+
+
+function clickHandler() {
+   var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+            
+           })
+        .catch(errorHandler)
+
+    function newFunction() {
+        return response => response.json();
+    }
 };
 
 btnTranslate.addEventListener("click", clickHandler)
